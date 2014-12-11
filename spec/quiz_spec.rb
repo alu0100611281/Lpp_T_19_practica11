5cb2ac21815d8c6d1e3e3390a317368ca1aa318a
alu0100611281 <jose@jose-SVE1512U1ESI>
2014-12-11 11:37:07 +0000

    añadiendo preguntas al spec

--- spec/quiz_spec.rb
+++ spec/quiz_spec.rb
@@ -6,7 +6,43 @@ module Quiz
      before :each do
 	@quiz = Quiz::Quest_Quiz.new("Cuestionario de LPP 05/12/2014") {
 
+	question 'Cual es el tipo del objeto en el siguiente codigo Ruby class Objeto',
+	    :right => 'Un Objeto',
+	    :wrong => []
+	    wrong 'Una instancia de la clase Class'
+	    wrong 'Una constante'
+	    wrong 'Ninguna de las anteriores'
 	
+	question 'Salida de:class Xyz tdef pots @nice tend end',
+	    :right => 'nil',
+	    :wrong => []
+	    wrong '#<Xyz:0x00000002bf0ed0'
+	    wrong '0'
+	    wrong 'Ninguna de las anteriores'
+	
+	question 'class Array def say_hi HEY! end end p[1,, bob].say_hi',
+	    :right => 'Ninguna de las anteriores',
+	    :wrong => []
+	    wrong '1'
+	    wrong 'bob'
+	    wrong 'HEY!'
+
+	question 'Cuantos argumentos de tipo bloque puede recibir un metodo?',
+	    :right => '1',
+	    :wrong => []
+	    wrong '2'
+	    wrong 'muchos'
+	    wrong 'los que defina el usuario'
+        
+	question 'Es apropiado que una clase Tablero herede de una clase Juego',
+	    :right=>'Cierto',
+	    :wrong => []
+	    wrong 'Falso'
+    
+	question 'Salida class de hash_raro = {[1, 2, 3] => Object.new(),Hash.new => :toto}',
+	    :right =>'Cierto',
+	    :wrong => []
+	    wrong 'Falso'
 	}
   end#end del before
 
